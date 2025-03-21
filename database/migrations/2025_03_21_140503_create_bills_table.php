7ff3d054c67a868f3540f4fbf71f5b6b62497bce
mkemaln <kemalmuhammad2509@gmail.com>
2025-03-21 21:15:23 +0700

    update migration for position, overtime, departement, and bills

---
+++ database/migrations/2025_03_21_140503_create_bills_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bills', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->decimal('amount', 10, 2);
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->date('deadline');
+            $table->uuid('payment_id')->nullable();
+            $table->enum('status', ['pending', 'paid', 'overdue'])->default('pending');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bills');
+    }
+};
