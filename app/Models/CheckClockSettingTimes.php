57d7e03a33c7e2ec5232df17bea8ad16ff0bc74f
mkemaln <kemalmuhammad2509@gmail.com>
2025-05-20 16:35:10 +0700

    fix: add a new column for reject reason

--- app/Models/CheckClockSettingTimes.php
+++ app/Models/CheckClockSettingTimes.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Testing\Fluent\Concerns\Has;
 
 class CheckClockSettingTimes extends Model
 {
     use HasFactory;
+    use SoftDeletes;
     // Specify the table name
     protected $table = 'check_clock_setting_times';
 
@@ -21,6 +23,8 @@ class CheckClockSettingTimes extends Model
         'break_start',
         'break_end',
     ];
+
+    protected $dates = ['deleted_at'];
 
     // Define the relationship with the CheckClockSettings model
     public function checkClockSetting()
