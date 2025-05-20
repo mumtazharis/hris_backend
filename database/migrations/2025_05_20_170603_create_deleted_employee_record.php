b573a8e2483b76ae21784b5f413ef06ad5fb9ae0
mkemaln <kemalmuhammad2509@gmail.com>
2025-05-20 17:24:35 +0700

    feat: add new table for employee log and bills

---
+++ database/migrations/2025_05_20_170603_create_deleted_employee_record.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('deleted_employee_log', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained('users'); // user that delete the data
+            $table->integer('deleted_employee'); // id of deleted employee
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('deleted_employee_log');
+    }
+};
